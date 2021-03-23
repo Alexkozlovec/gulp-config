9cb66d22b76a9b5e43e22dbdda65fffe24d2002f
Aleksandr Kozlovets <kozlovetsalexandr@gmail.com>
2021-03-23 11:34:05 +0200

    simplified structure, renamed js file

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,14 @@ const gcmq = require("gulp-group-css-media-queries");
 
 //images
 const imagemin = require("gulp-imagemin");
-const webp = require('gulp-webp');
+const webp = require("gulp-webp");
 
 // scripts
 const webpackStream = require("webpack-stream");
 const uglify = require("gulp-uglify");
 
 //overall
-const plumber = require('gulp-plumber');
+const plumber = require("gulp-plumber");
 const sourcemaps = require("gulp-sourcemaps");
 const del = require("del");
 const browserSync = require("browser-sync").create();
@@ -46,91 +46,118 @@ const html = () => {
     .pipe(gulpif(!isProd, browserSync.stream()));
 };
 
+const htmlBackend = () => {
+  return src(srcPath + "*.html").pipe(dest(distPath));
+};
+
 const images = () => {
   return src(srcPath + "images/**/*")
-  .pipe(gulpif(isProd, imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 3 }),
-      imagemin.svgo({
-        plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-      }),
-    ]))
-  ).pipe(dest(distPath + "images/"))
-  .pipe(gulpif(!isProd, browserSync.stream()))
-}
+    .pipe(
+      gulpif(
+        isProd,
+        imagemin([
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 85, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 3 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+          }),
+        ])
+      )
+    )
+    .pipe(dest(distPath + "images/"))
+    .pipe(gulpif(!isProd, browserSync.stream()));
+};
 
 const convertToWebp = () => {
   return src(srcPath + "images/**/*.{jpg, png}")
-  .pipe(webp({quality: 90}))
-  .pipe(dest(distPath + "images/"));
+    .pipe(webp({ quality: 90 }))
+    .pipe(dest(distPath + "images/"));
 };
 
 const fonts = () => {
   return src(srcPath + "fonts/**/*")
-  .pipe(dest(distPath + "fonts/"))
-  .pipe(gulpif(!isProd, browserSync.stream()));
-}
+    .pipe(dest(distPath + "fonts/"))
+    .pipe(gulpif(!isProd, browserSync.stream()));
+};
 
 const assets = () => {
   return src(srcPath + "assets/**/*")
-  .pipe(dest(distPath + "assets/"))
-  .pipe(gulpif(!isProd, browserSync.stream()))
-}
+    .pipe(dest(distPath + "assets/"))
+    .pipe(gulpif(!isProd, browserSync.stream()));
+};
 
 const styles = () => {
   return src(srcPath + "scss/style.scss")
-  .pipe(gulpif(!isProd, plumber()))
-  .pipe(gulpif(!isProd, sourcemaps.init()))
-  .pipe(sass())
-  .pipe(postcss([autoprefixer()]))
-  .pipe(gulpif(!isProd, sourcemaps.write(".")))
-  .pipe(gulpif(isProd, gcmq()))
-  .pipe(gulpif(isProd, cssnano({
-      zindex: false,
-      discardComments: {
-        removeAll: true,
-      },
-  })))
-  .pipe(dest(distPath + "css/"))
-  .pipe(gulpif(!isProd, browserSync.stream()))
-}
+    .pipe(gulpif(!isProd, plumber()))
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulpif(!isProd, sourcemaps.write(".")))
+    .pipe(gulpif(isProd, gcmq()))
+    .pipe(
+      gulpif(
+        isProd,
+        cssnano({
+          zindex: false,
+          discardComments: {
+            removeAll: true,
+          },
+        })
+      )
+    )
+    .pipe(dest(distPath + "css/"))
+    .pipe(gulpif(!isProd, browserSync.stream()));
+};
+
+const stylesBackend = () => {
+  return src(srcPath + "scss/style.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gcmq())
+    .pipe(dest(distPath + "css/"));
+};
 
 const scripts = () => {
   return src(srcPath + "scripts/*.js")
-  .pipe(gulpif(
-    isProd,
-    webpackStream({
-      mode: "production",
-      output: {
-        filename: "app.js",
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(js)$/,
-            exclude: /(node_modules)/,
-            loader: "babel-loader",
-            query: {
-              presets: ["@babel/preset-env"],
-            },
+    .pipe(
+      gulpif(
+        isProd,
+        webpackStream({
+          mode: "production",
+          output: {
+            filename: "main.js",
           },
-        ],
-      },
-    })
-  ))
-  .pipe(gulpif(isProd, uglify()))
-  .pipe(gulpif(
-    !isProd,
-    webpackStream({
-      mode: "development",
-      output: {
-        filename: "app.js",
-      },
-    })))
-  .pipe(dest(distPath + "scripts/"))
-  .pipe(gulpif(!isProd, browserSync.stream()));
-}
+          module: {
+            rules: [
+              {
+                test: /\.(js)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                  presets: ["@babel/preset-env"],
+                },
+              },
+            ],
+          },
+        })
+      )
+    )
+    .pipe(gulpif(isProd, uglify()))
+    .pipe(
+      gulpif(
+        !isProd,
+        webpackStream({
+          mode: "development",
+          output: {
+            filename: "main.js",
+          },
+        })
+      )
+    )
+    .pipe(dest(distPath + "scripts/"))
+    .pipe(gulpif(!isProd, browserSync.stream()));
+};
 
 const cleanDist = () => {
   return del("./" + distPath);
@@ -149,13 +176,27 @@ const watchFiles = () => {
 const setProd = (cb) => {
   isProd = true;
   cb();
-}
+};
 
 const watcher = parallel(watchFiles, serve);
 
-exports.default = series(cleanDist, parallel(html, fonts, images, assets, styles, scripts), watcher /*convertToWebp */);
+exports.default = series(
+  cleanDist,
+  parallel(html, fonts, images, assets, styles, scripts),
+  watcher /*convertToWebp */
+);
 
-exports.build = series(setProd, cleanDist, parallel(html, fonts, images, assets, styles, scripts));
+exports.backend = series(
+  setProd,
+  cleanDist,
+  parallel(htmlBackend, fonts, images, assets, stylesBackend, scripts)
+);
+
+exports.build = series(
+  setProd,
+  cleanDist,
+  parallel(html, fonts, images, assets, styles, scripts)
+);
 // exports
 
 exports.serve = serve;
